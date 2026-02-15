dce1bcfc15ed1377c63705d4f19ecdf32b876d88
Simon Praetorius <simon.praetorius@tu-dresden.de>
2026-02-15 15:24:41 +0100

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 setup(
     name="mkdocs-doxygen-plugin",
 
-    version="0.1.0",
+    version="0.1.1",
 
     description="Doxygen plugin for MkDocs",
     long_description=long_description,
